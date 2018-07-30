bea6841105ccaa0e2de4c6c8bb1779c055d02ff9
William Bryant <wilbryan@uat.edu>
2018-07-29 19:08:04 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,45 +1,53 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <ctime>
 #include <iostream>
-#include <string>
+#include <boost/array.hpp>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
-// function that creates a tring to be sent back to the client
-std::string make_daytime_string()
-{
-	using namespace std; // for time_t, time and ctime
-	time_t now = time(0); // store system time value 0
-	return ctime(&now);
-}
-
-int main()
+int main(int argc, char* argv[])
 {
 	try
 	{
-		boost::asio::io_service io_service;
-		// this object is created to listen for new connections
-		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
+		if (argc != 2)
+		{
+			std::cerr << "Usage: client <host>" << std::endl;
+			return 1;
+		}
 
+		boost::asio::io_service io_service;
+		tcp::resolver resolver(io_service); // turn into TCP endpoint (connecting out to)
+		tcp::resolver::query query(argv[1], "daytime"); // searching for the IP address of the server 
+		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query); // list of endpoints is returned using an iterator 
+
+																			 // create and connect the sockets. Since the endpoints above contain IPv4 and IPv6 endpoints, we 
+																			 // try each of them until one works. connect() will do this automatically 
+		tcp::socket socket(io_service);
+		boost::asio::connect(socket, endpoint_iterator);
+
+		// now the connection is open. We now need to read the response from the daytime service
+
+		// boost array holds and receives data. buffer() automatically determines the size of
+		// the array to help prevent buffer overruns
 		for (;;)
 		{
-			// this socket represents the connection to the client
-			tcp::socket socket(io_service);
-			// wait for connection
-			acceptor.accept(socket);
+			boost::array<char, 128> buf;
+			boost::system::error_code error;
 
-			// client connects to our service. determine current time 
-			// and tranfer info to client 
-			std::string message = make_daytime_string();
+			// when the server closes the connection, read_some() will exit with an error (:::eof),
+			// which is how we know to exit the loop
+			size_t len = socket.read_some(boost::asio::buffer(buf), error);
 
-			boost::system::error_code ignored_error;
-			boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+			if (error == boost::asio::error::eof)
+				break; // connection closed cleanly by peer
+			else if (error)
+				throw boost::system::system_error(error); // some other error
+
+			std::cout.write(buf.data(), len);
 		}
 	}
+	// handle any other exceptions
 	catch (std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
-	return 0;
 }
